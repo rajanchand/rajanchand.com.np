d39c9a849c3a4970a30423518ef5088e8f0c3206
rajan <rajanchand48@gmail.com>
2026-06-06 18:40:12 +0100

    updated dark mode theme and custom favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,7 +76,8 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
-          enableSystem
+          forcedTheme="dark"
+          enableSystem={false}
           disableTransitionOnChange
         >
           {children}
